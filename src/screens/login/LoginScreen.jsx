4adedda4c9352880eaa67682acc2344f1cd37cbb
GonzaloNavarrete <gon.navarrete@duocuc.cl>
2026-06-16 16:08:09 -0400

    Update LoginScreen.jsx

--- src/screens/login/LoginScreen.jsx
+++ src/screens/login/LoginScreen.jsx
@@ -16,7 +16,7 @@ export default function LoginScreen() {
     e.preventDefault();
 
     try {
-      const response = await fetch(import.meta.env.VITE_API_URL;, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
